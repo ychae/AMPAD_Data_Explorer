932687f8fec868e79c0aba63bf2b87fa3cdbf062
Kenneth Daily <kmdaily@gmail.com>
2014-10-14 15:42:59 -0700

    Added logging.

--- miRNA_data_prep.R
+++ miRNA_data_prep.R
@@ -1,17 +1,16 @@
 ###
 #get the PCBC samples raw miRNA counts
 ###
-cat('Reading the PCBC raw miRNA Exp data from Synapse')
+flog.info('Reading the PCBC raw miRNA Exp data from Synapse', name='synapse')
 miRNA_normCounts <- synGet('syn2701942')
 miRNA_normCounts <- read.delim(miRNA_normCounts@filePath, header=T, sep='\t', as.is=T, stringsAsFactors = F, check.names=F)
 temp_rownames <- tolower(miRNA_normCounts$mir)
 miRNA_normCounts$mir <- NULL
 miRNA_normCounts <- apply(miRNA_normCounts,2, function(x) as.numeric(x))
 rownames(miRNA_normCounts) <- temp_rownames
-cat('..Done\n\n')
 
 #get the miRNA to genes mapping table from synapse
-cat('Reading the miRNA to genes mapping table')
+flog.info('Reading the miRNA to genes mapping table from Synapse', name='synapse')
 miRNA_to_genes <- synGet('syn2246991')
 miRNA_to_genes <- read.delim(miRNA_to_genes@filePath, header=T, sep="\t", stringsAsFactors=FALSE, check.names=F)
 miRNA_to_genes$mirName <- tolower(miRNA_to_genes$Pathway)
@@ -33,7 +32,6 @@ temp_miRNAs_names['miRNAPrecursor'] <- unlist(lapply(strsplit(as.character(temp_
 miRNA_to_genes <- merge(temp_miRNAs_names, miRNA_to_genes, by.x='miRNAPrecursor', by.y='mirName', all.x=T)
 #remove dups
 miRNA_to_genes <- miRNA_to_genes[!duplicated(miRNA_to_genes),]
-cat('..Done\n\n')
 
 # 
 # x <- convert_to_ensemblIds(sample_gene_list)
@@ -58,11 +56,10 @@ cat('..Done\n\n')
 
 
 #miRNA metadata
-cat('Reading the PCBC  miRNA metadata')
+flog.info('Reading the PCBC miRNA metadata from Synapse', name='synapse')
 miRNA_metadata <- synGet('syn2731149') 
 miRNA_metadata <- read.delim(miRNA_metadata@filePath, header=T, sep='\t',as.is=T, stringsAsFactors = F, check.names=F)
 rownames(miRNA_metadata) <- miRNA_metadata$sample
 #keep only those samples that are present in the expression matrix
 rows_to_keep <- rownames(miRNA_metadata) %in% colnames(miRNA_normCounts)
 miRNA_metadata <- miRNA_metadata[rows_to_keep, ]
-cat('..Done\n\n')
