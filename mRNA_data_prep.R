5841784a8c029297c16a8482716860e6e21133ef
Kenneth Daily <kmdaily@gmail.com>
2014-10-15 10:26:07 -0700

    Formatting and logging.

--- mRNA_data_prep.R
+++ mRNA_data_prep.R
@@ -1,10 +1,15 @@
 ###
 #get the PCBC samples geneExp normalized counts
 ###
+
 flog.info('Reading the PCBC normalized mRNA Exp data from Synapse', name='synapse')
 mRNA_NormCounts <- synGet('syn2701943')
+
 #read in the file
-mRNA_NormCounts <- read.delim(mRNA_NormCounts@filePath, header=T, sep='\t', as.is=T, stringsAsFactors = F, check.names=F)
+mRNA_NormCounts <- read.delim(mRNA_NormCounts@filePath, header=T, sep='\t',
+                              as.is=T, stringsAsFactors = F, check.names=F)
+
+## remove version from ENSEMBL ID
 rownames(mRNA_NormCounts) <- gsub('\\..*', '',mRNA_NormCounts$gene_id)
 mRNA_NormCounts$symbol <- NULL
 mRNA_NormCounts$gene_id <- NULL
@@ -14,8 +19,9 @@ mRNA_NormCounts$locus <- NULL
 #rownames(mRNA_NormCounts)
 
 ###
-#get the metadata from the synapse for PCBC geneExp samples
+#get the metadata from synapse for PCBC geneExp samples
 ###
+flog.info('Reading the PCBC mRNA metadata from Synapse', name='synapse')
 mRNA_metadata <- synGet('syn2731147')
 mRNA_metadata <- read.delim(mRNA_metadata@filePath, header=T, sep='\t',as.is=T, stringsAsFactors = F, check.names=F)
 rownames(mRNA_metadata) <- mRNA_metadata[,'Decorated Name']
