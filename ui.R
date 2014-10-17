3d37d0b83397c3f065722cb0c7fee5e36d632187
Kenneth Daily <kmdaily@gmail.com>
2014-10-17 09:37:06 -0700

    Formatting.

--- ui.R
+++ ui.R
@@ -9,39 +9,59 @@ shinyUI( fluidPage(
     ##################################
     sidebarPanel(
       progressInit(),
+
       h4('1. Select a gene list'),
+      
       tabsetPanel(
         id = 'genelist_type',
         #TAB PANEL 1 : custom gene list
-        tabPanel('My Search',h5('1.a. Search on a custom gene list:'),
+        tabPanel('My Search',
+                 h5('1.a. Search on a custom gene list:'),
+                 
                  helpText("Accepts HUGO/Ensembl/Entrez gene ids"),
-                 tags$textarea(id="custom_gene_list",rows=8,cols=200,paste0(sample_gene_list, collapse=', ')),
+                 
+                 tags$textarea(id="custom_gene_list",
+                               rows=8, cols=200,
+                               paste0(sample_gene_list, collapse=', ')),
+                 
                  checkboxInput('incl_corr_genes', 'also include correlated genes', value = FALSE),
-                 sliderInput('corr_threshold', label='Correlation Threshold', min=0.5, max=1.0, value=0.9, step=0.05),
+                 
+                 sliderInput('corr_threshold', label='Correlation Threshold',
+                             min=0.5, max=1.0, value=0.9, step=0.05),
+                 
                  br(),
+                 
                  h5('1.b. Add miRNA Targets (mirbase ids):'),
                  tags$textarea(id="custom_miRNA_list",rows=4,cols=200),
+                 
                  br(),
+                 
                  actionButton("custom_search", h4("Search")),
+                 
                  br(),
+                 
                  value='custom_gene_list'
         ), #END TAB PANEL 1
+        
         # TAB PANEL 2 : select a pathway
         tabPanel(
           'Pathways',
           selectInput("selected_pathways",
                       "1.a. Select Pathway/s",
-                      choices = names(pathways_list), selectize=T, multiple=T, width='400px',
+                      choices = names(pathways_list),
+                      selectize=T, multiple=T, width='400px',
                       selected = names(pathways_list)[c(1:2)]),
           br(), br(), br(), br(),
           value='pathway'
         ),  #END TAB PANEL 2
+        
         #TAB PANEL 3 : precomputed sig gene list
         tabPanel(
           'Significant Genes',
           #tags$div(title="Significantly enriched gene lists as a result of pairwise comparison of all PCBC samples",
                    selectInput( "selected_Significant_GeneList",
-                                "Precomputed Significant gene lists (?)", selectize=FALSE, 
+                                "Precomputed Significant gene lists (?)",
+                                selectize=FALSE, 
                                 choices = sort(names(precomputed_enrichedPathways_in_geneLists)) #loaded from getDATA.R
                               ),
           #dynamically updated with TOOL TIP
@@ -57,36 +77,55 @@ shinyUI( fluidPage(
       ),#END TABSET 
   
       br(),br(),
+      
       #heatmap annotation labels
-      selectInput('heatmap_annotation_labels', h4('2. Annotate Samples by:'),
+      selectInput('heatmap_annotation_labels',
+                  h4('2. Annotate Samples by:'),
                   choices  = colnames(combined_metadata)[-1],  #-1 to remove the first value "Sample"
                   selected='Differentiation_State'),
+      
       br(),
       
       #FILTER OPTIONS
       h4('3. Filter samples by:'),
       #1. filter based on mod_linetype
-      selectInput('linetype', h5('Line type'), choices=unique(combined_metadata$Line_Type),
+      selectInput('linetype', h5('Line type'),
+                  choices=unique(combined_metadata$Line_Type),
                   selectize=T, multiple=T, selected=c('ESC','iPSC')),
-      selectInput('gene_combination', h5('Reprogramming Gene Combination'), choices=unique(combined_metadata$Reprogramming_Gene_Combination),
+      
+      selectInput('gene_combination', h5('Reprogramming Gene Combination'),
+                  choices=unique(combined_metadata$Reprogramming_Gene_Combination),
                   selectize=T, multiple=T),
-      selectInput('vector_type', h5('Reprogramming Vector Type'), choices=unique(combined_metadata$Reprogramming_Vector_Type),
+      
+      selectInput('vector_type', h5('Reprogramming Vector Type'),
+                  choices=unique(combined_metadata$Reprogramming_Vector_Type),
                   selectize=T, multiple=T),
-      selectInput('tissue_origin', h5('Tissue of Origin'), choices=unique(combined_metadata$Tissue_of_Origin),
+      
+      selectInput('tissue_origin', h5('Tissue of Origin'),
+                  choices=unique(combined_metadata$Tissue_of_Origin),
                   selectize=T, multiple=T),
-      selectInput('diff_state', h5('Differentiation State'), choices=unique(combined_metadata$Differentiation_State),
+      
+      selectInput('diff_state', h5('Differentiation State'),
+                  choices=unique(combined_metadata$Differentiation_State),
                   selectize=T, multiple=T),
-      selectInput('cell_origin', h5('Cell Type of Origin'), choices=unique(combined_metadata$Cell_Type_of_Origin),
+      
+      selectInput('cell_origin', h5('Cell Type of Origin'),
+                  choices=unique(combined_metadata$Cell_Type_of_Origin),
                   selectize=T, multiple=T),
-      selectInput('gender', h5('Gender'), choices=unique(combined_metadata$Gender),
+      
+      selectInput('gender', h5('Gender'),
+                  choices=unique(combined_metadata$Gender),
                   selectize=T, multiple=T),
       
       br(),
+      
       h4('4. Heatmap Settings:'),
+      
       #distance metric
-      selectInput("clustering_distance","Distance Calculation",
+      selectInput("clustering_distance", "Distance Calculation",
                   choices=c("correlation", "euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski"),
                   selectize=T, multiple=F, selected="correlation"),
+      
       #linkage 
       selectInput("clustering_method","Clustering Method",
                   choices=c("ward", "single", "complete", "average", "mcquitty", "median", "centroid"),
