a135d60abaecd34c6ea60071f336e9cbea3dde34
Kenneth Daily <kenneth.daily@sagebase.org>
2014-10-21 16:55:29 -0700

    Updated methylation mapping to filter here based on the variance in expression (which is done to the meth_data object in Synapse syn2731494), reading newly created meth_to_gene file.

--- methylation_data_prep.R
+++ methylation_data_prep.R
@@ -7,10 +7,11 @@ meth_data[,1] <- NULL
 
 #meth to gene annotation
 flog.info('Reading the PCBC methylation to genes mapping file', name='synapse')
-meth_to_gene <- synGet('syn2731501')
-meth_to_gene <- read.delim(meth_to_gene@filePath, header=T, sep='\t', as.is=T, stringsAsFactors = F, 
-                           check.names=F)
+meth_to_gene_file <- synGet('syn2775255')
+meth_to_gene <- read.delim(meth_to_gene_file@filePath, header=T, sep='\t', 
+                           as.is=T, stringsAsFactors = F, check.names=F)
 meth_to_gene$entrezID <-  as.character(meth_to_gene$entrezID)
+meth_to_gene <- subset(meth_to_gene, methProbeID %in% rownames(meth_data))
 
 #methylation metadata
 flog.info('Reading the PCBC methlyation metadata', name='synapse')
@@ -20,8 +21,3 @@ rownames(meth_metadata) <- meth_metadata$Sample
 #keep only those samples that are present in the expression matrix
 rows_to_keep <- rownames(meth_metadata) %in% colnames(meth_data)
 meth_metadata <- meth_metadata[rows_to_keep, ]
-
-
-
-
-
