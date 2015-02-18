2908d02c05dce02ae506cc9142a8db2b34e2eacd
Kenneth Daily <kenneth.daily@sagebase.org>
2015-02-17 22:30:31 -0800

    Use subset instead of filter to preserve rownames

--- global_functions.R
+++ global_functions.R
@@ -85,13 +85,13 @@ get_filtered_metadata <- function(input, metadata){
   filtered_metadata <- metadata
   
   if( length(input$linetype) != 0 ){
-    filtered_metadata <- filter(filtered_metadata, Line_Type %in% input$linetype)
+    filtered_metadata <- subset(filtered_metadata, Cell_Line_Type %in% input$linetype)
   }
   if( length(input$gene_combination) != 0 ){
-    filtered_metadata <- filter(filtered_metadata, Reprogramming_Gene_Combination %in% input$gene_combination)  
+    filtered_metadata <- subset(filtered_metadata, Reprogramming_Gene_Combination %in% input$gene_combination)  
   }
   if(length(input$vector_type) != 0){
-    filtered_metadata <- filter(filtered_metadata, Reprogramming_Vector_Type %in% input$vector_type)
+    filtered_metadata <- subset(filtered_metadata, Reprogramming_Vector_Type %in% input$vector_type)
   }
   if(length(input$tissue_origin) != 0){
     filtered_metadata <- subset(filtered_metadata, Tissue_of_Origin %in% input$tissue_origin)
