394bfc1bc70733a3f5751ba7448d5d648b6c6a81
kdaily <kdaily@users.noreply.github.com>
2015-05-12 12:43:57 -0700

    formatting

--- miRNA_data_prep.R
+++ miRNA_data_prep.R
@@ -1,32 +1,35 @@
-###
-#get the PCBC samples raw miRNA counts
-###
+# get the PCBC samples raw miRNA counts
 flog.info('Reading the PCBC raw miRNA Exp data from Synapse', name='synapse')
+
 miRNA_normCounts <- synGet('syn2701942')
-miRNA_normCounts <- read.delim(miRNA_normCounts@filePath, header=T, sep='\t', as.is=T, stringsAsFactors = F, check.names=F)
+miRNA_normCounts <- read.delim(miRNA_normCounts@filePath, header=T, sep='\t', 
+                               as.is=T, stringsAsFactors = F, check.names=F)
+
 temp_rownames <- tolower(miRNA_normCounts$mir)
 miRNA_normCounts$mir <- NULL
-miRNA_normCounts <- apply(miRNA_normCounts,2, function(x) as.numeric(x))
+# miRNA_normCounts <- apply(miRNA_normCounts,2, function(x) as.numeric(x))
 rownames(miRNA_normCounts) <- temp_rownames
 
-#get the miRNA to genes mapping table from synapse
+# get the miRNA to genes mapping table from synapse
 flog.info('Reading the miRNA to genes mapping table from Synapse', name='synapse')
 miRNA_to_genes <- synGet('syn2246991')
-miRNA_to_genes <- read.delim(miRNA_to_genes@filePath, header=T, sep="\t", stringsAsFactors=FALSE, check.names=F)
+miRNA_to_genes <- read.delim(miRNA_to_genes@filePath, header=T, sep="\t", 
+                             stringsAsFactors=FALSE, check.names=F)
 miRNA_to_genes$mirName <- tolower(miRNA_to_genes$Pathway)
 miRNA_to_genes$SystemCode <- NULL
 miRNA_to_genes$Pathway <- NULL
 miRNA_to_genes$mirName <- tolower(gsub('\\*', '', miRNA_to_genes$mirName))
 miRNA_to_genes$mirName <- gsub('-.p', '', miRNA_to_genes$mirName)
 
+# match the miRNA exp matrix row names to target genes
 
+# split the paired miRNA name and use the second name
+temp_miRNAs_names <- as.data.frame(do.call('rbind', strsplit(rownames(miRNA_normCounts),',')), 
+                                   stringsAsFactors = F)
 
-####
-#match the miRNA exp matrix row names to target genes
-#####
-#split the paired miRNA name and use the second name
-temp_miRNAs_names <- as.data.frame(do.call('rbind',strsplit(rownames(miRNA_normCounts),',')), stringsAsFactors = F)
-temp_miRNAs_names <- as.data.frame(apply(temp_miRNAs_names,2,tolower),stringAsFactors=F)
+temp_miRNAs_names <- as.data.frame(apply(temp_miRNAs_names, 2, tolower),
+                                   stringAsFactors=F)
+
 colnames(temp_miRNAs_names) <- c('miRNA1', 'miRNA2')
 temp_miRNAs_names['miRNAPrecursor'] <- unlist(lapply(strsplit(as.character(temp_miRNAs_names[,'miRNA1']),split='-'), function(x) paste(x[1:3], collapse='-')))
 miRNA_to_genes <- merge(temp_miRNAs_names, miRNA_to_genes, by.x='miRNAPrecursor', by.y='mirName', all.x=T)
