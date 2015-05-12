5241cac68dc2317aad5827910da5ae5158631fb7
kdaily <kdaily@users.noreply.github.com>
2015-05-12 12:53:00 -0700

    extra steps to pre-process mirna to gene mapping to facilitate subsetting miRNA eset

--- miRNA_data_prep.R
+++ miRNA_data_prep.R
@@ -31,8 +31,18 @@ temp_miRNAs_names <- as.data.frame(apply(temp_miRNAs_names, 2, tolower),
                                    stringAsFactors=F)
 
 colnames(temp_miRNAs_names) <- c('miRNA1', 'miRNA2')
-temp_miRNAs_names['miRNAPrecursor'] <- unlist(lapply(strsplit(as.character(temp_miRNAs_names[,'miRNA1']),split='-'), function(x) paste(x[1:3], collapse='-')))
-miRNA_to_genes <- merge(temp_miRNAs_names, miRNA_to_genes, by.x='miRNAPrecursor', by.y='mirName', all.x=T)
+
+temp_miRNAs_names <- cbind(original=rownames(miRNA_normCounts), 
+                           temp_miRNAs_names)
+
+## remove any suffix of -3p or -5p
+temp_miRNAs_names['miRNAPrecursor'] <- unlist(lapply(strsplit(as.character(temp_miRNAs_names[,'miRNA1']), 
+                                                              split='-'), 
+                                                     function(x) paste(x[1:3], collapse='-')))
+
+miRNA_to_genes <- merge(temp_miRNAs_names, miRNA_to_genes, 
+                        by.x='miRNAPrecursor', by.y='mirName', all.x=T)
+
 #remove dups
 miRNA_to_genes <- miRNA_to_genes[!duplicated(miRNA_to_genes),]
 
@@ -60,8 +70,9 @@ eset.miRNA <- ExpressionSet(assayData=as.matrix(miRNA_normCounts),
                             phenoData=AnnotatedDataFrame(miRNA_metadata),
                             featureData=AnnotatedDataFrame(miRNA_features))
 
+## Add separate miRNA names to the featureData
 foo <- fData(eset.miRNA) %>% 
-  separate(explicit_rownames, c("first", "second"),
+  separate(explicit_rownames, c("miRNA1", "miRNA2"),
            sep=",", extra="error", remove=FALSE)
 
 rownames(foo) <- rownames(fData(eset.miRNA))
