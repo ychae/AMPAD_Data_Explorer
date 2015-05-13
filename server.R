0c9d9054cb0ede0fae6e032acd45798587d20697
kdaily <kdaily@users.noreply.github.com>
2015-05-13 13:31:49 -0700

    updated comment

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ clean_list <- function(x, change_case=toupper) {
   # Split by space, comma or new lines
   x <- unlist(strsplit(x, split=c('[\\s+,\\n+\\r+)]'),perl=T))
   
-  # convert everything to upper case
+  # convert everything to specified case
   x <- change_case(x)
   
   # remove the blank entries
