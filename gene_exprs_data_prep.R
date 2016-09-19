71113d690b4cec5fd53f2369c7e9d8185456b5e5
Yooree Chae <ychae@users.noreply.github.com>
2016-09-19 10:48:10 -0700

    Update gene_exprs_data_prep.R

--- gene_exprs_data_prep.R
+++ gene_exprs_data_prep.R
@@ -18,7 +18,7 @@ library(githubr)
 library(biomaRt)
 library(ComplexHeatmap)
 
-synapseLogin()
+# synapseLogin()
 
 # Utility function to download tsv or csv file from synapse and load it in to memory
 downloadFile <- function(id, ...){
